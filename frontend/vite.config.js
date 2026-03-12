f5309b1a847705da4b26ac21200640846a9db28d
Rafail-Drakakis <rafaildrakakis123@gmail.com>
2026-03-12 08:37:12 +0200

    added conf to run host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,      // allow access from network
     port: 5173,
     proxy: {
       '/api': {
